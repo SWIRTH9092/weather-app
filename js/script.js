8a4ff4d35ce80cba021011907b3a9ecbf94a8188
Your Name <you@example.com>
2022-10-18 09:05:58 -0400

    html updated

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
 
-let movieData, userInput;
+// Constants and Variables
+const API_KEY = '';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
-const $title = $('#title');
-const $year = $('#year');
-const $rated = $('#rated');
-const $input = $('input[type="text"]');
 
-$('form').on('submit', handleGetData);
 
-function handleGetData(event) {
-    event.preventDefault();
-       // calling preventDefault() on a 'submit' event will prevent a page refresh  
-    userInput = $input.val();
-      // getting the user input
-    $.ajax({
-        url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
-      }).then(
-        (data) => {
-         movieData = data;
-         render();
-        },
-        (error) => {
-         console.log('bad request', error);
-        }
-    );    
+//  current weather doc //
+// let movieData, userInput;
+// const $title = $('#title');
+// const $cityName = $('cityName');
+// const $rated = $('#rated');
+// const $input = $('input[type="text"]');
+
+// $('form').on('submit', handleGetData);
+
+// //Event Listener
+// function handleGetData(event) {
+//     event.preventDefault();
+//        // calling preventDefault() on a 'submit' event will prevent a page refresh  
+//     userInput = $input.val();
+//       // getting the user input
+//     $.ajax({
+//         url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
+//       }).then(
+//         (data) => {
+//          movieData = data;
+//          render();
+//         },
+//         (error) => {
+//          console.log('bad request', error);
+//         }
+//     );    
+// }
+
+// function render() {
+//     $title.text(movieData.Title);
+//     $year.text(movieData.Year);
+//     $rated.text(movieData.Rated);
+//  }
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+function handleGetData() {
+  $.ajax(BASE_URL + 'q=Martinsburg&appid=' + API_KEY)
+  .then(function(data) { 
+    console.log("data", data);
+  }, function(error) {
+    console.log('error:', error);
+  })
 }
 
-function render() {
-    $title.text(movieData.Title);
-    $year.text(movieData.Year);
-    $rated.text(movieData.Rated);
- }
+ // 
