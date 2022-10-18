f81a3ba3af328eb7cba5b9df0a64184a7e7bf06c
Your Name <you@example.com>
2022-10-18 10:52:51 -0400

    added imperial for temp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // Constants and Variables
-const API_KEY = 'a6e9bad4fa160aa7de5ed9f6e4088082';
+const API_KEY = '';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 let weatherData, userInput;
@@ -15,41 +15,19 @@ const $input = $('input[type="text"]');
 // Event Listener for form Input
 $('form').on('submit', handleGetData);
 
-// //Event Listener
-// function handleGetData(event) {
-//     event.preventDefault();
-//        // calling preventDefault() on a 'submit' event will prevent a page refresh  
-//     userInput = $input.val();
-//       // getting the user input
-//     $.ajax({
-//         url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
-//       }).then(
-//         (data) => {
-//          movieData = data;
-//          render();
-//         },
-//         (error) => {
-//          console.log('bad request', error);
-//         }
-//     );    
-// }
-
-// function render() {
-//     $title.text(movieData.Title);
-//     $year.text(movieData.Year);
-//     $rated.text(movieData.Rated);
-//  }
-
-
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 //Handle function for Input
 function handleGetData (event) {
+  // calling preventDefault() on a 'submit' event will    prevent a page refresh  
   event.preventDefault();
 
+  //  getting User Input
   userInput = $input.val();
   console.log("user Input", userInput)
-  $.ajax(BASE_URL + 'q=Martinsburg&appid=' + API_KEY)
+
+  // Calling API for weather information
+  $.ajax(BASE_URL + 'q=Martinsburg&appid=' + API_KEY + '&units=imperial')
   .then(
     (data) => { 
     weatherData = data;
@@ -61,9 +39,11 @@ function handleGetData (event) {
   })
 }
 
+// Update HTML Text
 function render() {
     console.log("weatherData.name", weatherData.name)
-    $weatherCity.text(`Weather For: ${weatherData.name}`);
+    $weatherCity.text(`Weather For: ${weatherData.name}`);   
+
 //     $year.text(movieData.Year);
 //     $rated.text(movieData.Rated);
  }
