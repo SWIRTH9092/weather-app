c9a45a216d3fc07227aace964093398351508310
Your Name <you@example.com>
2022-10-20 22:08:31 -0400

    added geo api and error mesages

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# weather-app
+# Weather-App
+
+
+This project uses 2 API's from openweathermap. org to display the weather for a city, state
+
+-  Geocoding API.  City and state code from the input form is used to access the latitude and longitute for the weather. The format of the API is below:
+
+        http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+-  Current weather Data API.  The longitude and latitude from the GEOcoding API is used to find the current weather to display on the screen.  The format of the API is below (in the API call the paramter:  &units=imperial was added at the end so that the app would return the tempature in fahrenheit):   
+
+        https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+
+
+#### Technologies Used
+
+- HTML
+- CSS.  
+- Javascript
+- Jquery. 
+
+Features:
+- Used Grid layout for responsive design
+- - Used Jquery to make the API calls and for Dom Manipulation. 
+- Edited the input data for having both city and state before making API calls
+- Generated errors messages: 
+    -   When there was not a city and state coded in the input field.
+    -   When the GEO API did not find a longitude and latitude for an input city, State code
+
+### Stretch Goals
+-  Add more weather information to the window
+-  Allow the user to how many days of forcasts they want
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 
 // Constants and Variables
-const API_KEY = 'a6e9bad4fa160aa7de5ed9f6e4088082';
+const API_Key = 'a6e9bad4fa160aa7de5ed9f6e4088082';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
-let weatherData, userInput;
+
+let weatherData, geoData, userInput, cityName, stateCode, srchComma, baseGEO_API;
+
+const baseGEO_URL = 'https://api.openweathermap.org/geo/1.0/direct?q=' 
+
 
 // jquery variables for HTML elements
 const $weatherCity = $('#wcity');
@@ -12,53 +16,82 @@ const $feelsLike = $('#feelslike')
 const $weather = $('#weather')
 const $input = $('input[type="text"]');
 
+// Function to handle error for not finding weather
+function errorHandling (errorMessage) {
+  $weatherCity.text(errorMessage).css("color", "red")
+  $temp.text("")
+  $feelsLike.text("")
+  $weather.text ("")  
+}
+
 // Event Listener for form Input
 $('form').on('submit', handleGetData);
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
 //Handle function for Input
 function handleGetData (event) {
-  // calling preventDefault() on a 'submit' event will    prevent a page refresh  
+// calling preventDefault() on a 'submit' event will    prevent a page refresh  
   event.preventDefault();
 
   //  getting User Input and reset to placeholder
   userInput = $input.val();
+  srchComma = userInput.search(",")
+
+  //Edit input city, state
+  // if error found, display error.message
+  if (srchComma > 0) {
+      cityName = userInput.substring(0, srchComma).trim()
+      stateCode = userInput.substring(srchComma + 1, userInput.length)
+  } else {
+    //Function to handle error for not finding weather
+      errorHandling('Error in Input Format: City Name, State Code')
+    return
+  }
+  
+  //  Reset Input value
   $input.val("")
-    // Calling API for weather information
-  $.ajax(`${BASE_URL}q=${userInput}&appid=${API_KEY}&units=imperial`)
 
-  // $.ajax(BASE_URL + 'q=Martinsburg&appid=' + API_KEY + '&units=imperial')
-  .then(
-    (data) => { 
-    weatherData = data;
-    render();
-  }, 
-    (error) => {
-    //  checkiing for city not found
-    if (error.status === 404 || error.status === 404) {
-      $weatherCity.text(`Invalid City Name: ${userInput} `)
-      $temp.text("")
-      $feelsLike.text("")
-      $weather.text ("")  
-     } else {
-    // all other errors log....  
-      console.log("error", error)
-    }
+  // Call to get Geo Data
+  const promise1 = $.ajax(baseGEO_URL + `${cityName},${stateCode.trim()},&appid=${API_Key}`);
+   promise1.then(
+     (gData) => { 
+         geoData = gData
 
-  })
+      // check to see if the city-state code was found
+        //  if no log error message and return
+      if (geoData.length === 0) {
+         errorHandling(`City Name, State Code not found: ${userInput}`)
+         $temp.text("")
+         return
+        }
+
+      //  Save city name for output
+      cityName = geoData[0].name  
+
+      // now read weather by longitude-latitude   
+      const promise2 = $.ajax(BASE_URL + `lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${API_Key}&units=imperial`)
+      promise2.then(
+        (data) => {
+            weatherData = data;
+            render();
+          }, 
+        (error) => {
+            console.log("error", error)
+          }
+        )
+      },
+      (error) => {
+        console.log("error", error)
+      })
 }
 
 // Update HTML Text
 function render() {
     $weatherCity.text
-        (`${weatherData.name} Weather`);   
+        (`${cityName} Weather`).css("color", "#091D34")  
     $temp.text
         (`Temperature: ${Math.round(weatherData.main.temp)}°`)
     $feelsLike.text
         (`Feels Like:  ${Math.round(weatherData.main.feels_like)}°`)
     $weather.text
-        (`Weather:  ${(weatherData.weather[0].description
-          )}`)
-
- }
+        (`Weather:  ${(weatherData.weather[0].description)}`)
+  }
